046267837fa550573e71ef1d04c468f3d5b0b3f9
Luc Perkins <lucperkins@gmail.com>
2018-01-28 14:57:43 -0800

    create javascript pipeline

--- themes/jaeger-docs/gulpfile.js
+++ themes/jaeger-docs/gulpfile.js
@@ -2,6 +2,8 @@ const gulp     = require('gulp'),
       sass     = require('gulp-sass'),
       hash     = require('gulp-hash'),
       prefixer = require('gulp-autoprefixer'),
+      uglify   = require('gulp-uglify'),
+      concat   = require('gulp-concat'),
       del      = require('del');
 
 const SRCS = {
@@ -13,6 +15,33 @@ const DIST = {
   css: 'static/css',
   js: 'static/js'
 }
+
+gulp.task('js', (done) => {
+  gulp.src(SRCS.js)
+    .pipe(uglify())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(DIST.js));
+
+  done();
+});
+
+gulp.task('js-dev', (done) => {
+  del(['static/js/app-*.js']);
+
+  gulp.src(SRCS.js)
+    .pipe(uglify())
+    .pipe(concat('app.js'))
+    .pipe(hash())
+    .pipe(gulp.dest(DIST.js))
+    .pipe(hash.manifest('js.json'))
+    .pipe(gulp.dest('data'));
+
+  done();
+});
+
+gulp.task('js:watch', () => {
+  gulp.watch(SRCS.js, gulp.series('js-dev'));
+});
 
 gulp.task('sass', (done) => {
   gulp.src(SRCS.sass)
@@ -30,11 +59,11 @@ gulp.task('sass-dev', (done) => {
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
-    .pipe(hash())
     .pipe(prefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
+    .pipe(hash())
     .pipe(gulp.dest(DIST.css))
     .pipe(hash.manifest('css.json'))
     .pipe(gulp.dest('data'));
@@ -46,6 +75,6 @@ gulp.task('sass:watch', () => {
   gulp.watch(SRCS.sass, gulp.series('sass-dev'));
 });
 
-gulp.task('build', gulp.series('sass'));
+gulp.task('build', gulp.series('js', 'sass'));
 
-gulp.task('dev', gulp.series('sass-dev', gulp.parallel('sass:watch')));
+gulp.task('dev', gulp.series('js-dev', 'sass-dev', gulp.parallel('js:watch', 'sass:watch')));
